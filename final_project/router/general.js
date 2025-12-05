7166f73ca1e0e27f2099ef279235877713b2db5b
klckerim <klcabdulkerim@gmail.com>
2025-12-05 15:19:10 -0500

    Using Promises or Async-Await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,16 +1,17 @@
 const express = require('express');
+const axios = require("axios");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
+public_users.post("/register", (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    if(!isValid(username)){
-        return res.status(404).json({message: "Unable to register user."});
+    if (!isValid(username)) {
+        return res.status(404).json({ message: "Unable to register user." });
     }
 
     if (username && password) {
@@ -19,33 +20,64 @@ public_users.post("/register", (req,res) => {
         });
 
         if (userswithsamename.length == 0) {
-            users.push({"username": username, "password": password});
-            return res.status(200).json({message: "User successfully registered. Now you can login"});
-        }else {
-            return res.status(404).json({message: "User already exists!"});
+            users.push({ "username": username, "password": password });
+            return res.status(200).json({ message: "User successfully registered. Now you can login" });
+        } else {
+            return res.status(404).json({ message: "User already exists!" });
         }
     }
-    return res.status(404).json({message: "Unable to register user."});
+    return res.status(404).json({ message: "Unable to register user." });
 });
+
+//Task 10
+async function getBooksAsync() {
+    const response = await axios.get("http://localhost:5000/");
+    return response.data;
+}
+getBooksAsync()
+    .then(data => console.log(data))
+    .catch(err => console.error("Error:", err));
+
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.send(JSON.stringify(books,null,4));
+public_users.get('/', function (req, res) {
+    return res.send(JSON.stringify(books, null, 4));
 });
 
+//Task 11
+async function getBooksDetailsAsync() {
+    const response = await axios.get("http://localhost:5000/isbn/4");
+    return response.data;
+}
+getBooksDetailsAsync()
+    .then(data => console.log(data))
+    .catch(err => console.error("Error:", err));
+
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-    return res.send(JSON.stringify(books[req.params.isbn],null,4));
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    return res.send(JSON.stringify(books[req.params.isbn], null, 4));
+});
+
+//Task 12
+function getBooksByAuthorPromise(author) {
+    axios.get(`http://localhost:5000/author/${author}`)
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(err => {
+            console.error("Error fetching by author:", err);
+        });
+}
+getBooksByAuthorPromise("Chinua Achebe");
 
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', function (req, res) {
     const author = req.params.author;
     const bookKeys = Object.keys(books);
     let results = [];
-    for(let key of bookKeys){
+    for (let key of bookKeys) {
         let book = books[key];
-        if(book.author === author){
+        if (book.author === author) {
             results.push({
                 id: key,
                 ...book
@@ -55,14 +87,26 @@ public_users.get('/author/:author',function (req, res) {
     return res.send(results);
 });
 
+//Task 13
+function getBooksByTitlePromise(title) {
+    axios.get(`http://localhost:5000/title/${title}`)
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(err => {
+            console.error("Error fetching by title:", err);
+        });
+}
+getBooksByTitlePromise("The Divine Comedy");
+
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', function (req, res) {
     const title = req.params.title;
     const bookKeys = Object.keys(books);
     let results = [];
-    for(let key of bookKeys){
+    for (let key of bookKeys) {
         let book = books[key];
-        if(book.title === title){
+        if (book.title === title) {
             results.push({
                 id: key,
                 ...book
@@ -73,11 +117,11 @@ public_users.get('/title/:title',function (req, res) {
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get('/review/:isbn', function (req, res) {
     let book = books[req.params.isbn];
-    
 
-    return res.status(300).json({review: book["reviews"]});
+
+    return res.status(300).json({ review: book["reviews"] });
 });
 
 module.exports.general = public_users;
