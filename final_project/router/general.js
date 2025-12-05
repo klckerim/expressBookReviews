a1cf203fe3b95d8f455b3bdf79707d689ed03cf5
klckerim <klcabdulkerim@gmail.com>
2025-12-05 11:21:35 -0500

    Update and test the general user routes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -56,8 +56,10 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let book = books[req.params.isbn];
+    
+
+    return res.status(300).json({review: book["reviews"]});
 });
 
 module.exports.general = public_users;
